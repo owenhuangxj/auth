0cbaea7563fa00ee86e8517f91fdb8f56fee3ff9
owenhuangxj <wangluoke007@163.com>
2019-01-23 14:48:45 +0800

    修改了WebConfig,静态资源jquery找不到！

--- src/main/java/com/ss/config/WebConfig.java
+++ src/main/java/com/ss/config/WebConfig.java
@@ -3,23 +3,43 @@ package com.ss.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import jdk.nashorn.internal.runtime.QuotedStringTokenizer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@Configuration
+@EnableWebMvc//表示该类是做Spring MVC 配置的
+@Configuration //表示将该类注册到Spring IoC容器中去
+@ComponentScan(value = "com.ss.controller")
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("=========================addCorsMappings=========================");
+        registry.addMapping("/**")
+                .allowCredentials(true) // 是否将凭证和数据一起提交到服务器
+                .allowedHeaders("*")    //
+                .allowedOrigins("*")    // 本服务器的所有资源都可以被访问，默认也是所有资源
+                .maxAge(600);
+    }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("=========================addResourceHandlers=========================");
+        // 以/resources/** 为格式的资源都映射到static目录下去
+        registry.addResourceHandler("resources/**")
+                .addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX.concat("/static/**"))
+                .setCachePeriod(60*60*24*7);
     }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        System.out.println("=========================configureMessageConverters=========================");
         FastJsonHttpMessageConverter messageConverter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
         config.setSerializerFeatures(
@@ -29,9 +49,10 @@ public class WebConfig implements WebMvcConfigurer {
                 SerializerFeature.WriteNullListAsEmpty,             // 空List 输出为[]
                 SerializerFeature.WriteNullStringAsEmpty,           // 空字符串输出为""
                 SerializerFeature.WriteNullBooleanAsFalse,          // 空布尔值输出为false
-                SerializerFeature.WriteDateUseDateFormat,           // 
-                SerializerFeature.DisableCircularReferenceDetect    //
+                SerializerFeature.WriteDateUseDateFormat,           // 使用日期转化器
+                SerializerFeature.DisableCircularReferenceDetect    // 禁止循环引用
         );
-
+        messageConverter.setFastJsonConfig(config);
+        converters.add(messageConverter);
     }
 }
